e1e452eb25d5458e8a5414fa9aaf86cb51f66abb
vandan <vandankhamkar1@gmail.com>
2023-03-03 18:09:30 +0530

    init

--- components/RelatedProducts.js
+++ components/RelatedProducts.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'next/link';
-const card1 = '../card/card-1.png';
+const card2 = '../card/card-2.png';
+const card3 = '../card/card-3.png';
+const card4 = '../card/card-4.png';
 export default function RelatedProducts() {
   const Card = (props) => {
     return (
@@ -21,13 +23,13 @@ export default function RelatedProducts() {
     <div className='card-wrapper container'>
       <div className='row gy-5 gx-5'>
         <div className='col-12 col-md-4'>
-          <Card title="Rosy Haze Heart Pendant Necklace in Rose Gold Plated 925 Silver" src={card1} salePrice="₹19000" originalPrice="₹21000" />
+          <Card title="Oxidised Owning My Perfectionist Tendencies Necklace" src={card2} salePrice="₹9999" originalPrice="₹11000" />
         </div>
         <div className='col-12 col-md-4'>
-          <Card title="Rosy Haze Heart Pendant Necklace in Rose Gold Plated 925 Silver" src={card1} salePrice="₹19000" originalPrice="₹21000" />
+          <Card title="Little Star Kids' Gold Earrings" src={card3} salePrice="₹33990" originalPrice="₹35000" />
         </div>
         <div className='col-12 col-md-4'>
-          <Card title="Rosy Haze Heart Pendant Necklace in Rose Gold Plated 925 Silver" src={card1} salePrice="₹19000" originalPrice="₹21000" />
+          <Card title="Glitzy Wild Fern Necklace " src={card4} salePrice="₹11990" originalPrice="₹13000" />
         </div>
       </div>
     </div>
